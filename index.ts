c5a900aa5a1323d51c15a25999a6ac36680ff187
silverwind <me@silverwind.io>
2025-09-29 01:04:45 +0200

    workaround tsdown bug

--- index.ts
+++ index.ts
@@ -1,13 +1,9 @@
-import {realpathSync} from "node:fs";
 import {fileURLToPath} from "node:url";
 import type {Options} from "tsdown";
 
 export type CustomConfig = Options & {url: string};
 
 const base = ({url, ...other}: CustomConfig): Options => {
-  console.info(fileURLToPath(new URL("index.ts", url)));
-  console.info(realpathSync("index.ts"));
-
   return {
     entry: fileURLToPath(new URL("index.ts", url)),
     report: {
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,4 +3,5 @@ import {nodeLib} from "./index.ts";
 
 export default defineConfig(nodeLib({
   url: import.meta.url,
+  entry: "index.ts", // https://github.com/rolldown/tsdown/issues/518
 }));
