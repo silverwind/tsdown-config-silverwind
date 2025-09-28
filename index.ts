7ed7f2b966a90b5a74104b364b6d9859df7d9e4a
silverwind <me@silverwind.io>
2025-09-28 23:05:52 +0200

    add webLib export

--- index.ts
+++ index.ts
@@ -1,20 +1,28 @@
 import {fileURLToPath} from "node:url";
 import type {Options} from "tsdown";
 
-export type CustomConfig = {url: string} & Options;
+export type CustomConfig = Options & {url: string};
 
 const base = ({url, ...other}: CustomConfig): Options => ({
   entry: fileURLToPath(new URL("index.ts", url)),
+  report: {
+    gzip: false,
+    brotli: false,
+  },
   ...other,
 } satisfies Options);
 
 export function nodeLib({url, ...other}: CustomConfig): Options {
   return base({
     platform: "node",
-    report: {
-      gzip: false,
-      brotli: false,
-    },
+    url,
+    ...other,
+  });
+}
+
+export function webLib({url, ...other}: CustomConfig): Options {
+  return base({
+    platform: "browser",
     url,
     ...other,
   });
